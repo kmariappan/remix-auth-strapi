ac3e7b1ab3d15c6ad19adea9abc5c92b71642088
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2022-04-29 23:03:07 +0200

    Initial Commit

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,38 @@
-import { SessionStorage } from "@remix-run/server-runtime";
+/* eslint-disable prettier/prettier */
+import { SessionStorage } from "@remix-run/server-runtime"
 import {
   AuthenticateOptions,
   Strategy,
   StrategyVerifyCallback,
-} from "remix-auth";
+} from "remix-auth"
 
 /**
  * This interface declares what configuration the strategy needs from the
  * developer to correctly work.
  */
-export interface MyStrategyOptions {
-  something: "You may need";
+export interface StrapiStrategyOptions {
+  something: "You may need"
 }
 
 /**
  * This interface declares what the developer will receive from the strategy
  * to verify the user identity in their system.
  */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
+export interface StrapiStrategyVerifyParams {
+  something: "Dev may need"
 }
 
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
+export class MyStrategy<User> extends Strategy<
+  User,
+  StrapiStrategyVerifyParams
+> {
+  name = "change-me"
 
   constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
+    options: StrapiStrategyOptions,
+    verify: StrategyVerifyCallback<User, StrapiStrategyVerifyParams>
   ) {
-    super(verify);
+    super(verify)
     // do something with the options here
   }
 
@@ -37,12 +41,7 @@ export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
     sessionStorage: SessionStorage,
     options: AuthenticateOptions
   ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
+    return await this.failure("Implement me!", request, sessionStorage, options)
     // Uncomment me to do a success response
     // this.success({} as User, request, sessionStorage, options);
   }
