013d96b94346e5ce36474adce4bc47a9cf98b6d5
Karthikeyan Mariappan <karthikeyanmariappan@hotmail.com>
2022-05-02 22:12:00 +0200

    Strapi Strategy Base

--- src/index.ts
+++ src/index.ts
@@ -5,44 +5,82 @@ import {
   Strategy,
   StrategyVerifyCallback,
 } from "remix-auth"
+import { StrapiClient } from "@kmariappan/strapi-client-js"
+import { AuthData } from "@kmariappan/strapi-client-js/src/lib/types/auth"
+import { StrapiApiResponse } from "@kmariappan/strapi-client-js/src/lib/types/base"
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
+export type StrapiAuthResponse = StrapiApiResponse<AuthData>
+
 export interface StrapiStrategyOptions {
-  something: "You may need"
+  readonly strapiClient: StrapiClient
+
+  readonly sessionStorage: SessionStorage
+
+  readonly sessionKey?: string
+
+  readonly sessionErrorKey?: string
 }
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface StrapiStrategyVerifyParams {
-  something: "Dev may need"
+export interface VerifyParams {
+  readonly request: Request
+
+  readonly strapiClient: StrapiClient
 }
 
-export class MyStrategy<User> extends Strategy<
-  User,
-  StrapiStrategyVerifyParams
-> {
-  name = "change-me"
+export class StrapiStragegy extends Strategy<StrapiAuthResponse, VerifyParams> {
+  name = "strapi"
+
+  readonly sessionKey: string
+  readonly sessionErrorKey: string
+
+  private readonly strapiClient: StrapiClient
+  private readonly sessionStorage: SessionStorage
 
   constructor(
     options: StrapiStrategyOptions,
-    verify: StrategyVerifyCallback<User, StrapiStrategyVerifyParams>
+    verify: StrategyVerifyCallback<StrapiAuthResponse, VerifyParams>
   ) {
+    if (!options?.strapiClient)
+      throw new Error(
+        "StrapiStrategy : Constructor expected to receive a strapi client instance. Missing options.strapiClient"
+      )
+
+    if (!options?.sessionStorage)
+      throw new Error(
+        "StrapiStrategy : Constructor expected to receive a session storage instance. Missing options.sessionStorage"
+      )
+    if (!verify)
+      throw new Error(
+        "StrapiStrategy : Constructor expected to receive a verify function. Missing verify"
+      )
+
     super(verify)
-    // do something with the options here
+
+    this.strapiClient = options.strapiClient
+    this.sessionStorage = options.sessionStorage
+    this.sessionKey = options.sessionKey ?? "strapi-auth:session"
+    this.sessionErrorKey = options.sessionErrorKey ?? "strapi-auth:error"
   }
 
   async authenticate(
     request: Request,
     sessionStorage: SessionStorage,
     options: AuthenticateOptions
-  ): Promise<User> {
-    return await this.failure("Implement me!", request, sessionStorage, options)
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+  ): Promise<StrapiAuthResponse> {
+    const authResponse = await this.verify({
+      request,
+      strapiClient: this.strapiClient,
+    })
+
+    return authResponse.data?.jwt
+      ? this.success(authResponse, request, sessionStorage, options)
+      : this.failure(
+          authResponse.error?.message
+            ? authResponse.error?.message
+            : "no user found",
+          request,
+          sessionStorage,
+          options
+        )
   }
 }
